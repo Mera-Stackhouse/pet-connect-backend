3f8771cd98ecc781b6497647244b7ce1ee531a9b
Mera-Stackhouse <mera.stackhouse@gmail.com>
2019-06-17 09:16:04 -0700

    making create action in event controller

--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -6,8 +6,21 @@ class Api::V1::EventsController < ApplicationController
   end
 
   def create
-    @event.create(event_params)
-    render json: @event, status: :accepted
+    event = Event.new(event_params)
+    params["event"]["users"].each{|u|
+      EventsUser.new(event: event, user_id: u.id)
+    }
+    params["event"]["pets"].each{|p|
+      EventsPet.new(event: event, pet_id: p.id)
+    }
+    debugger
+
+    # @event = Event.create(event_params)
+    #
+    # params.event.users.each{|u|
+    #   EventsUser.create(event: @event, user_id: u.id)
+    # }
+    # render json: @event, status: :accepted
   end
 
   def update
@@ -28,7 +41,7 @@ class Api::V1::EventsController < ApplicationController
   end
 
   def event_params
-    params.require(:event).permit(:location, :start_time, :event_type)
+    params.require(:event).permit(:location, :start_time, :event_type, :users, :pets)
   end
 
 end
