8f6da475008127e27d2a68538382e929e79394b5
Mera-Stackhouse <mera.stackhouse@gmail.com>
2019-06-19 16:44:08 -0700

    cleaning up

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,7 +2,6 @@ class Api::V1::UsersController < ApplicationController
 
   def index
     users = User.all
-  
     render json: users
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,13 +6,13 @@ class User < ApplicationRecord
   has_many :events, through: :events_users
 
 
-  def self.from_omniauth(auth)
-    # Creates a new user only if it doesn't exist
-    where(email: auth.info.email).first_or_initialize do |user|
-      user.name = auth.info.name
-      user.email = auth.info.email
-    end
-  end
+  # def self.from_omniauth(auth)
+  #   # Creates a new user only if it doesn't exist
+  #   where(email: auth.info.email).first_or_initialize do |user|
+  #     user.name = auth.info.name
+  #     user.email = auth.info.email
+  #   end
+  # end
 
 
 end
