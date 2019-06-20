b228069e521466850a029fe1ebc42fe88cd4e19d
Mera-Stackhouse <mera.stackhouse@gmail.com>
2019-06-19 20:16:30 -0700

    adds user relationships routes and controller actions

---
+++ app/controllers/api/v1/user_relationships_controller.rb
@@ -0,0 +1,32 @@
+class UserRelationshipsController < ApplicationController
+
+  def create
+    get_ids
+    relationship = UserRelationship.create(follower_id: @follower.id, followed_id: @followed.id)
+    render json: relationship, status: :accepted
+  end
+
+  def update
+    get_ids
+    relationship_id = UserRelationship.where(follower_id: @follower.id, followed_id: @followed.id).pluck(:id)
+    relationship = UserRelationship.find(relationship_id)
+    relationship.update(confirmed: true)
+    render json: relationship, status: :accepted
+  end
+
+  def destroy
+    get_ids
+    relationship_id = UserRelationship.where(follower_id: @follower.id, followed_id: @followed.id).pluck(:id)
+    relationship = UserRelationship.find(relationship_id)
+    relationship.destroy
+    render json: {message: 'deleted'}, status: :accepted
+  end
+
+  private
+
+  def get_ids
+    @follower = User.find(params[:follower_id]]
+    @followed = User.find(params[:follower_id]]
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
       resources :events, only: [:show, :create, :update]
       resources :pets, only: [:create, :update, :index, :destroy]
       resources :users, only: [:show, :create, :update, :index]
+      resources :user_relationships, only: [ :create, :update, :destroy]
       # get '/profile/:id', to: 'users#profile'
 
       get 'users/friends/:id', to: 'users#friends'
